d3e1cba12d65b6f5a7699ddf17d08763225a1795
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-10-06 17:57:20 -0400

    Enable upgrades

--- LHGames/Bot/Behaviours/UpgradeBehaviour.cs
+++ LHGames/Bot/Behaviours/UpgradeBehaviour.cs
@@ -6,10 +6,29 @@ namespace LHGames.Bot.Behaviours
 {
     public class UpgradeBehaviour : Behaviour
     {
-        private Stack<UpgradeType> _upgradeList;
+        private Stack<UpgradeType> _upgradeList = new Stack<UpgradeType>();
         private int[] _costs;
 
         public UpgradeBehaviour(BehaviourExecuter executer) : base(executer)
+        {
+
+        }
+
+        public override bool Evaluate()
+        {
+            if (_costs == null)
+            {
+                Setup();
+            }
+            return _executer.PlayerInfo.Position == _executer.PlayerInfo.HouseLocation && _executer.PlayerInfo.TotalResources >= _costs[_executer.PlayerInfo.GetUpgradeLevel(_upgradeList.Peek())];
+        }
+
+        public override string Execute()
+        {
+            return AIHelper.CreateUpgradeAction(_upgradeList.Pop());
+        }
+
+        private void Setup()
         {
             var upgraded = new Dictionary<UpgradeType, int>();
             upgraded.Add(UpgradeType.AttackPower, _executer.PlayerInfo.GetUpgradeLevel(UpgradeType.AttackPower));
@@ -24,27 +43,18 @@ namespace LHGames.Bot.Behaviours
             _upgradeList.Push(UpgradeType.CarryingCapacity);
             _upgradeList.Push(UpgradeType.CollectingSpeed);
 
-            var ok = true;
-            while (ok)
+            while (true)
             {
                 if (upgraded[_upgradeList.Peek()] > 0)
                 {
                     var up = _upgradeList.Pop();
                     upgraded[up]--;
+                    continue;
                 }
+                break;
             }
 
-            _costs = new int[]{10000, 15000, 25000, 50000, 100000};
-        }
-
-        public override bool Evaluate()
-        {
-            return _executer.PlayerInfo.Position == _executer.PlayerInfo.HouseLocation && _executer.PlayerInfo.TotalResources >= _costs[_executer.PlayerInfo.GetUpgradeLevel(_upgradeList.Peek())];
-        }
-
-        public override string Execute()
-        {
-            return AIHelper.CreateUpgradeAction(_upgradeList.Pop());
+            _costs = new int[] { 10000, 15000, 25000, 50000, 100000 };
         }
     }
 }
