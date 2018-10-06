d38c2c2620500986c83aa888b6e12d0653829c7d
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-10-06 12:47:58 -0400

    Add hardoded mining behaviour

--- LHGames/Bot/Behaviours/MiningBehaviour.cs
+++ LHGames/Bot/Behaviours/MiningBehaviour.cs
@@ -5,7 +5,9 @@ namespace LHGames.Bot.Behaviours
 {
     public class MiningBehaviour : Behaviour
     {
-        public MiningBehaviour(BehaviourExecuter executer) : base(executer) {}
+        public MiningBehaviour(BehaviourExecuter executer) : base(executer) { }
+
+        private bool isGoingHome = false;
 
         public override bool Evaluate()
         {
@@ -14,7 +16,24 @@ namespace LHGames.Bot.Behaviours
 
         public override string Execute()
         {
-            return AIHelper.CreateEmptyAction();
+            if (_executer.Map.GetTileAt(_executer.PlayerInfo.Position.X, _executer.PlayerInfo.Position.Y) == TileContent.House)
+            {
+                isGoingHome = false;
+            }
+            else if (_executer.PlayerInfo.CarriedResources == _executer.PlayerInfo.CarryingCapacity ||
+                _executer.Map.GetTileAt(_executer.PlayerInfo.Position.X + 1, _executer.PlayerInfo.Position.Y) == TileContent.Wall)
+            {
+                isGoingHome = true;
+            }
+            if (_executer.PlayerInfo.CarriedResources < _executer.PlayerInfo.CarryingCapacity && _executer.Map.GetTileAt(_executer.PlayerInfo.Position.X + 1, _executer.PlayerInfo.Position.Y) == TileContent.Resource)
+            {
+                return AIHelper.CreateCollectAction(new Point(1, 0));
+            }
+            else
+            {
+                int direction = isGoingHome ? -1 : 1;
+                return AIHelper.CreateMoveAction(new Point(direction, 0));
+            }
         }
     }
 }
